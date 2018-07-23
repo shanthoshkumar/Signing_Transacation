5c0b6927ab62b9773ec78226953a06979e48e614
shanthoshkumar <shanthoshkumar1008@gmail.com>
2018-07-23 11:10:13 +0530

    Change

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,23 +12,20 @@ import * as Tx from 'ethereumjs-tx';
 export class HomeComponent {
   title = 'app';
   web3 = null;
-  account;balance;transaction_count;code_at;GBTC;estimated_gas;_nonce;value_before;value_after
+  private_key = "cd67a2cbe1e4a555f7b6da3ef2fb357dc026943cc5c2bf5b85c48b8348a600d0"
+  privateKey = Buffer.from(this.private_key, 'hex');
+  account;balance;transaction_count;code_at;GBTC;
 
-  constructor() {}
-  
-
-  set(key){
-    // alert(key)  
-    var private_key = key;
-    var  privateKey = Buffer.from(private_key, 'hex');
-    this.web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/Vr1GWcLG0XzcdrZHWMPu'));   
+  constructor() {
+    this.web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/Vr1GWcLG0XzcdrZHWMPu'));
+    let account = '0x4d8d095fc69Afedd24aA121f75C0Fa43D33F3706'; // Ganache
     let contractAddress = '0x44524dfd5d99240c659795cc0466b9d485174c56'; // Deployed manually
     let abi = [{"constant":true,"inputs":[],"name":"a","outputs":[{"name":"","type":"uint32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_a","type":"uint32"}],"name":"set","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"get","outputs":[{"name":"","type":"uint32"}],"payable":false,"stateMutability":"view","type":"function"}];
 
-    // const contract = new this.web3.eth.Contract(abi, contractAddress, {
-    //   from: account,
-    //   gasLimit: 3000000,
-    // });
+    const contract = new this.web3.eth.Contract(abi, contractAddress, {
+      from: account,
+      gasLimit: 3000000,
+    });
     
     // const contractFunction = contract.methods.set(18);
     // const functionAbi = contractFunction.encodeABI();
@@ -36,69 +33,56 @@ export class HomeComponent {
     // let estimatedGas;
     // let nonce;
     
-   
-   
-   try {
+    // console.log("Getting gas estimate");
+    
+    // contractFunction.estimateGas({from: account}).then((gasAmount) => {
+    //   estimatedGas = gasAmount.toString(16);
+    
+    //   console.log("Estimated gas: " + estimatedGas);
+    
+    //   this.web3.eth.getTransactionCount(account).then(_nonce => {
+    //     nonce = _nonce.toString(16);
+    
+    //     console.log("Nonce: " + nonce);
+    //     const txParams = {
+    //       gasPrice: '493E0',
+    //       gasLimit: 4000000,
+    //       to: contractAddress,
+    //       data: functionAbi,
+    //       from: account,
+    //       nonce: '0x' + nonce
+    //     };
+    
+    //     const tx = new Tx(txParams);
+    //     tx.sign(this.privateKey);
+    
+    //     const serializedTx = tx.serialize();
+    
+    //     contract.methods.get().call().then(v => console.log("Value before increment: " + v));
+    
+    //     this.web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex')).on('receipt', receipt => {
+    //       console.log(receipt);
+    //       contract.methods.get().call().then(v => console.log("Value after increment: " + v));
+    //     })
+    //   });
+    // });
+
+
+    try {
       let meta = this;  
-      const obj = this.web3.eth.accounts.privateKeyToAccount("0x"+private_key);
+      const obj = this.web3.eth.accounts.privateKeyToAccount("0x"+this.private_key);
       console.log(obj);
       
       this.account = obj["address"];
 
-      const contract = new this.web3.eth.Contract(abi, contractAddress, {
-        from: this.account,
-        gasLimit: 3000000,
-      });
-
-      const contractFunction = contract.methods.set(18);
-      const functionAbi = contractFunction.encodeABI();
-  
-      let estimatedGas;
-      let nonce;
-      console.log("Getting gas estimate");
-    
-      contractFunction.estimateGas({from: this.account}).then((gasAmount) => {
-        estimatedGas = gasAmount.toString(16); 
-        this.estimated_gas=estimatedGas
-      
-        console.log("Estimated gas: " + estimatedGas);})
-      
-
-
       this.web3.eth.getBalance(this.account).then(bal => this.balance = bal );
       this.web3.eth.getTransactionCount(this.account).then(tcount => this.transaction_count = tcount);
-      this.web3.eth.getCode(this.account).then(code_at => this.code_at = code_at)
+      this.web3.eth.getCode("0x44524dfd5d99240c659795cc0466b9d485174c56").then(code_at => this.code_at = code_at)
       this.web3.eth.getBlockTransactionCount("latest",function(err,data){
         meta.GBTC = data; 
       })
 
-      contract.methods.get().call().then(v =>this.value_before=v);
-      this.web3.eth.getTransactionCount(this.account).then(_Nonce => {
-        this._nonce = _Nonce.toString(16);
-    
-        console.log("Nonce: " + this._nonce);
-       
-        const txParams  = {
-          nonce: '0x' + nonce,
-          gasPrice:  '493E0',
-          gasLimit: 4000000,
-          to: contractAddress,
-          value: '0x00',
-          data: functionAbi
-        }
 
-        const tx = new Tx(txParams);
-        tx.sign(privateKey);
-        var serializedTx = tx.serialize();
-        const temp_data=serializedTx.toString('hex');
-        console.log(temp_data);
- 
-      this.web3.eth.sendSignedTransaction('0x' + temp_data).on('receipt', console.log);
-
-        contract.methods.get().call().then(v => this.value_after= v);
-
-
-  })
       
     } catch (error) {
       console.log(error);
